e8cafa275834c99714ff63fe3719b9c72f46f434
JernejTDO <jernej.mip@gmail.com>
2021-10-12 22:23:51 +0200

    Update

--- src/main/java/xyz/aquacode/skyblock/main/MainSB.java
+++ src/main/java/xyz/aquacode/skyblock/main/MainSB.java
@@ -5,16 +5,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MainSB extends JavaPlugin
 {
 
+    public static MainSB plugin;
+
     @Override
     public void onEnable()
     {
-        // Enbale
+        plugin = this;
     }
 
     @Override
     public void onDisable()
     {
         // Disable
+    }
+
+    public static void ConsoleLog(Player player, String msg)
+    {
+
     }
 }
 
