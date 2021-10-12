7bd0c4d667d56ba6a2715dcc72c2e3b4481ca331
JernejTDO <jernej.mip@gmail.com>
2021-10-12 21:54:21 +0200

    Update

---
+++ src/main/java/xyz/aquacode/skyblock/main/MainSB.java
@@ -0,0 +1,18 @@
+package xyz.aquacode.skyblock.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainSB extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        // Enbale
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Disable
+    }
+}
