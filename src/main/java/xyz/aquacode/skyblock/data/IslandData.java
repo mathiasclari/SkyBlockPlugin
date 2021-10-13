01d93047d27d97758e3ead4814e1c180fa5d359c
JernejTDO <jernej.mip@gmail.com>
2021-10-13 17:09:29 +0200

    Update4

---
+++ src/main/java/xyz/aquacode/skyblock/data/IslandData.java
@@ -0,0 +1,67 @@
+package xyz.aquacode.skyblock.data;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class IslandData
+{
+    // Island Data
+    private String islandUUID;
+    private String worldName;
+    private List<UUID> islandMembers = new ArrayList<>();
+    private int islandSize;
+
+    // Island Save Data
+    public IslandData(String isUUID, String isWorld, List<UUID> isMembers, int isSize)
+    {
+        this.islandUUID = isUUID;
+        this.worldName = isWorld;
+        this.islandMembers = isMembers;
+        this.islandSize = isSize;
+    }
+
+    // Island Get Data
+    public String getIslandUUID()
+    {
+        return islandUUID;
+    }
+
+    public String getIslandWord()
+    {
+        return worldName;
+    }
+
+    public List<UUID> getIslandMembers()
+    {
+        return islandMembers;
+    }
+
+    public  int getIslandSize()
+    {
+        return islandSize;
+    }
+
+    // Set Island Data
+    public void setIslandWorld(String wn)
+    {
+      this.worldName = wn;
+    }
+
+    public void setIslandSize(int iss)
+    {
+        this.islandSize = iss;
+    }
+
+    public void removeIslandMembers(UUID userUUID)
+    {
+        this.islandMembers.remove(userUUID);
+    }
+
+    public void addIslandMember(UUID userUUID)
+    {
+        this.islandMembers.add(userUUID);
+    }
+}
--- src/main/java/xyz/aquacode/skyblock/main/MainSB.java
+++ src/main/java/xyz/aquacode/skyblock/main/MainSB.java
@@ -2,16 +2,25 @@ package xyz.aquacode.skyblock.main;
 
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.aquacode.skyblock.data.IslandData;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.UUID;
 
 public class MainSB extends JavaPlugin
 {
 
     public static MainSB plugin;
+    public static MySql dataBase;
+    public static Map<UUID, IslandData> IslandSaves = new HashMap<>();
 
     @Override
     public void onEnable()
     {
         plugin = this;
+        dataBase = new MySql("", "", "", "");
     }
 
     @Override
