75183881d02bd995109464cd7748ca5da4bb1f5a
JernejTDO <jernej.mip@gmail.com>
2021-10-15 16:17:11 +0200

    New Update

---
+++ src/main/java/xyz/aquacode/skyblock/scorebord/playerChat.java
@@ -0,0 +1,31 @@
+package xyz.aquacode.skyblock.scorebord;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import xyz.aquacode.skyblock.data.PlayerData;
+import xyz.aquacode.skyblock.main.MainSB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class playerChat implements Listener
+{
+    @EventHandler
+    public void onTalk(AsyncPlayerChatEvent event)
+    {
+        List<String> badWords = new ArrayList<>();
+
+        badWords.add("fuck");
+
+        if(badWords.contains(event.getMessage().toLowerCase()))
+        {
+            PlayerData pData = MainSB.PlayerSaves.get(event.getPlayer().getUniqueId());
+            event.setFormat("[" + pData.getPlayerScore() + "] " + MainSB.getChat().getPlayerPrefix(event.getPlayer()) + " " + event.getPlayer().getName() + ": " + event.getMessage());
+        }
+        else
+        {
+            MainSB.PlayerMessager(event.getPlayer(), "Your message was stopped.");
+        }
+    }
+}
