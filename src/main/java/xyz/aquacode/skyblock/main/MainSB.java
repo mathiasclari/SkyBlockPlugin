83302cb31474309133c4a3e063b4a4b55a12e4a6
JernejTDO <jernej.mip@gmail.com>
2021-10-15 16:02:58 +0200

    New Update

--- src/main/java/xyz/aquacode/skyblock/main/MainSB.java
+++ src/main/java/xyz/aquacode/skyblock/main/MainSB.java
@@ -1,11 +1,11 @@
 package xyz.aquacode.skyblock.main;
 
+import net.milkbowl.vault.chat.Chat;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-import xyz.aquacode.skyblock.ChatSystem.ChatMessages;
 import xyz.aquacode.skyblock.IslandManamger.IslandLoader;
-import xyz.aquacode.skyblock.Util.VaultUtils;
 import xyz.aquacode.skyblock.data.IslandData;
 import xyz.aquacode.skyblock.data.PlayerData;
 import xyz.aquacode.skyblock.data.PlayerLoader;
@@ -22,6 +22,7 @@ public class MainSB extends JavaPlugin
     public static MySql dataBase;
     public static Map<UUID, IslandData> IslandSaves = new HashMap<>();
     public static Map<UUID, PlayerData> PlayerSaves = new HashMap<>();
+    private static Chat chat = null;
 
     @Override
     public void onEnable()
@@ -32,21 +33,23 @@ public class MainSB extends JavaPlugin
 
         Bukkit.getPluginManager().registerEvents(new PlayerLoader(), this);
 
-        //Vault
-        VaultUtils.enableVault();
-        //
-        //ChatSystem
-        new ChatMessages(this);
-        instance = this;
-
+        setupChat();
     }
-
-    public static MainSB instance;
 
     @Override
     public void onDisable()
     {
         // Disable
+    }
+
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    public static Chat getChat() {
+        return chat;
     }
 
     public static void ConsoleLog(String msg)
